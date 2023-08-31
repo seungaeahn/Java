539319b86b716beaab19580dd5719625a4aa765b
seungaeahn <143372285+seungaeahn@users.noreply.github.com>
2023-08-31 17:51:15 +0900

    for / while

---
+++ Practice/src/com/kh/practice/exam/Practice9.java
@@ -0,0 +1,19 @@
+package com.kh.practice.exam;
+
+import java.util.Scanner;
+
+public class Practice9 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		
+		for() {
+			//+=사용하기
+		}
+		System.out.println();
+	}
+
+}
