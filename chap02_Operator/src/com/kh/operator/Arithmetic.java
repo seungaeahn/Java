06950c7551abe1aa087688798cfea83fbdf04582
seungaeahn <143372285+seungaeahn@users.noreply.github.com>
2023-09-01 10:51:06 +0900

    operator

--- chap02_Operator/src/com/kh/operator/Arithmetic.java
+++ chap02_Operator/src/com/kh/operator/Arithmetic.java
@@ -1,7 +1,7 @@
 package com.kh.operator;
-
+  
 public class Arithmetic {
-
+ 
 	public static void main(String[] args) {
 		// 1. 산술연산자 : 더하기(+), 빼기(-), 곱하기(*), 나머지 몫(/), 나누기 나머지(%)
 		int num1 = 5;
